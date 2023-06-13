19f10e449aba8a7cb91e9b4740c006356df82138
marcies smith <marcies.smith@gmail.com>
2023-06-13 19:24:39 -0400

    working on styling

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ const App = () => {
         <h2>About Me</h2>
         {/* Add recent photo or avatar and a short bio about the developer */}
         <div>
-        <img src="../img/profilepic.png" alt="Developer Avatar" />
+        <img src="../img/profilepic.png" alt="Developer Avatar" style={{ width: '25%', height: 'auto' }} />
+
 
           <p>I am a military veteran with a demonstrated track record of excellence in various roles. Currently employed as an IT Specialist at the US Environmental Protection Agency, I provide support in application software development and coding. I have certifications in IT with commitment to continuous academic growth. My leadership skills and commitment to personal and professional development make me a valuable asset. I have experience managing programs and ensuring compliance in the healthcare sector. With strong problem-solving, communication, and technical skills, I excel in delivering results and contributing to organizational success. </p>
         </div>
@@ -83,91 +84,91 @@ const App = () => {
         />
         <Project
           title="SVG Logo Maker"
-          image="Module10.jpg"
+          image="../img/svg.png"
           description="This application can be used to create a logo using SVG elements. This application uses Node.js, Inquirer, and SVG.js."
           deployedLink="https://drive.google.com/file/d/1So-WQX4MdkvM_4VQFqJXwZ4IC_YpS-UC/view"
           githubLink="https://github.com/Seicram/SVG-Logo-Maker-"
         />
         <Project
           title="Note Taker"
-          image="Module11.jpg"
+          image="../img/notetaker.png"
           description="This application can be used to write, save, and delete notes. This application uses an express backend and save and retrieve note data from a JSON file."
           deployedLink="https://note-taker-smith.herokuapp.com/"
           githubLink="https://github.com/Seicram/Note-Taker"
         />
         <Project
           title="Employee Tracker"
-          image="Module12.jpg"
+          image="../img/employee.png"
           description="This application can be used to manage a company's employees using node, inquirer, and MySQL. This application allows the user to view and manage the departments, roles, and employees in the company."
           deployedLink=" https://drive.google.com/file/d/1Ab8MbkMVzPaKA-zCN4lqUoPPHxh4etNb/view"
           githubLink="https://github.com/Seicram/Employee-Tracker"
         />
         <Project
           title="E-commerce Back End"
-          image="Module13.jpg"
+          image="../img/ecommerce.png"
           description="This application can be used to manage the back end of an e-commerce site. This application uses Express.js API and Sequelize to interact with a MySQL database."
           deployedLink="https://example.com/project3"
           githubLink="https://github.com/Seicram/E-commerce-Back-End"
         />
         <Project
           title="Tech Blog"
-          image="Module14.jpg"
+          image="../img/tech.png"
           description="This application is a CMS-style blog site similar to a Wordpress site, where developers can publish their blog posts and comment on other developers’ posts as well."
-          deployedLink="https://example.com/techblog"
+          deployedLink="https://tech-blog-smith.herokuapp.com/"
           githubLink="https://github.com/Seicram/Tech-Blog"
         />
         <Project
           title="Strain Finder"
-          image="Project2.jpg"
+          image="../img/p2.png"
           description="This project is a web application that allows users to search for cannabis strains and dispensaries in their area. This application uses the Leafly API and the Google Maps API. This application uses Node.js, Express.js, Handlebars.js, and Sequelize."
           deployedLink=" https://strain-finder-app.herokuapp.com"
           githubLink="https://github.com/A-Witthohn/Strain-Finder.git"
         />
         <Project
           title="Regex Tutorial"
-          image="Module17.jpg"
+          image="../img/regex.png"
           description="This application is a tutorial that explains the function of a regular expression. This application is deployed on GitHub gist."
           deployedLink="https://gist.github.com/Seicram/9a2f63204a69ee063f33db3f23ee1cd0"
           githubLink="https://github.com/Seicram/Regex-Tutorial"
         />
         <Project
           title="Social Network API"
-          image="Module18.jpg"
+          image="../img/social.png"
           description="This application is a social network API that uses a NoSQL database. This application allows users to share their thoughts, react to friends’ thoughts, and create a friend list."
           deployedLink="https://example.com/Social Network API"
           githubLink="https://github.com/Seicram/Social-Network-API"
         />
         <Project
           title="Text Editor"
-          image="Module19.jpg"
+          image="../img/inprogress.png"
           description="This application is a text editor that allows the user to create, edit, and save text files. This application uses Node.js, Electron, and the Ace editor."
           deployedLink="https://example.com/Text Editor"
           githubLink="https://github.com/Seicram/Text-Editor"
         />
         <Project
           title="React Portfolio"
-          image="Module20.jpg"
+          image="../img/react.png"
           description="This application is a portfolio that uses React. This application uses React components, JSX, and React props."
           deployedLink="https://example.com/React Portfolio"
           githubLink="https://github.com/Seicram/React-Portfolio"
         />
         <Project
           title="Book Search Engine"
-          image="Module21.jpg"
+          image="../img/inprogress.png"
           description="This application is a book search engine that uses the Google Books API. This application uses the MERN stack with a React front end, MongoDB database, and Node.js/Express.js server and API."
           deployedLink="https://example.com/Book Search Engine"
           githubLink="https://example.com/Book-Search-Engine"
         />
         <Project
           title="Extra Credit Assignment"
-          image="Module22.jpg"
+          image="../img/inprogress.png"
           description="This application is an extra credit assignment."
           deployedLink="https://example.com/Extra Credit Assignment"
           githubLink="https://example.com/Extra Credit Assignment"
         />
         <Project
           title="Project 3"
-          image="Project3.jpg"
+          image="../img/inprogress.png"
           deployedLink="https://example.com/project3"
           githubLink="https://example.com/Project-3"
         />
